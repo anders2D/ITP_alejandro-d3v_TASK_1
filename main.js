41448028df3be66e0bbfc3692ff63266844e250a
alejandro-d3v <alejandro.develop3r@gmail.com>
2023-09-02 15:53:35 -0500

    Archivos modificados

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-// Obtenemos el botón y el elemento de mensaje
 const myButton = document.getElementById("myButton");
 const message = document.getElementById("message");
 
-// Agregamos un evento de clic al botón
 myButton.addEventListener("click", function() {
-    // Cambiamos el mensaje cuando se hace clic en el botón
-    message.textContent = "¡Hola, Mundo!";
+    message.textContent = "¡Hola, Mundo de Humanos!";
 });
